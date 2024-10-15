4abc0c1522196670aab35db43a900fbf6a9014e6
Scott Morse <scott.morse@globalmusicrights.com>
2024-10-15 13:13:25 -0500

    Make changes to reproduce issue

--- rspack.config.js
+++ rspack.config.js
@@ -1,5 +1,9 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
+  const htmlPlugin = config.plugins.find(
+    (plugin) => plugin.constructor.name === 'HtmlRspackPlugin'
+  );
+  console.log('HtmlRspackPlugin _args: ', htmlPlugin._args);
   return config;
 });
